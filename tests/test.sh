39364e6d548691d1010188d15e5a3cdc8e8f2334
akazitoo <akds@cesar.school>
2021-11-29 22:24:56 -0300

    testes att

--- tests/test.sh
+++ tests/test.sh
@@ -4,4 +4,6 @@ read -p "Informe o seu nome de usuário: " usuario
 
 read -p "Você é funcionário da cantina? digite 's' para sim ou digite 'n' para não - " option2
 
-echo "DADOS : $option : $usuario : $option2 "
+read -p "Informe sua senha: " password
+
+echo "DADOS : $option : $usuario : $option2 : $password "
